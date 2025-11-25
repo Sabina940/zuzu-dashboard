bd46bd8f6c5f6529405ac97afb05789f1c322f25
Pierina Lopez <sabilop@Pierinas-MacBook-Air.local>
2025-11-25 15:16:32 +0100

    reminders update

--- src/pages/RemindersPage.tsx
+++ src/pages/RemindersPage.tsx
@@ -1,5 +1,5 @@
 // src/pages/RemindersPage.tsx
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import type { Reminder } from "../types";
 import {
   apiGetReminders,
